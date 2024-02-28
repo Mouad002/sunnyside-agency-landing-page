3b1487e902346464221ee1a588e636a217a03ba3
mouad kaaib <elcosto02@gmail.com>
2024-02-28 10:01:55 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var button = document.getElementById('button');
+var menu = document.getElementById('menu');
+var isVisible = false;
+
+button.addEventListener('click', (e) => {
+    console.log('clicked');
+    menu.classList.toggle('display-tender');
+    
+});
+
+
+
+
+
